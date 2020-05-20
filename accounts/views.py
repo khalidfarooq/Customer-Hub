7b499e5f36c2aef73a77f9f01e2dcb7c28952125
khalidfarooq <wandererkhalid@gmail.com>
2020-05-20 14:38:47 +0530

    Profile Section and User Relations defined

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.models import Group
 
 # Create your views here.
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import unauthenticated_user,allowed_users,admin_only
 
@@ -27,6 +27,9 @@ def registerPage(request):
 
              group = Group.objects.get(name='customer')
              user.groups.add(group)
+             Customer.objects.create(
+                user = user,
+             )
 
              messages.success(request,'Account was created for '+username)
              return redirect('login')
@@ -70,12 +73,21 @@ def home(request):
                'delivered':delivered}
     return render(request,'accounts/dashboard.html',context)
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    totalorders = orders.count()
+    pending = orders.filter(status = 'Pending').count()
+    delivered = orders.filter(status = 'Delivered').count()
+
+    context = {'orders': orders,'totalorders':totalorders,
+    'pending':pending,'delivered':delivered}
     return render(request, 'accounts/user.html', context)
 
-@allowed_users(allowed_roles=['admin'])
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def customer(request,pk):
     customer = Customer.objects.get(id = pk)
     orders = customer.order_set.all() #order_set is to query the data of child model
@@ -87,6 +99,22 @@ def customer(request,pk):
     ,'myFilter':myFilter}
 
     return render(request,'accounts/customer.html',context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+	customer = request.user.customer
+	form = CustomerForm(instance=customer)
+
+	if request.method == 'POST':
+		form = CustomerForm(request.POST, request.FILES,instance=customer)
+		if form.is_valid():
+			form.save()
+
+
+	context = {'form':form}
+	return render(request, 'accounts/account_settings.html', context)
+
 
 @allowed_users(allowed_roles=['admin'])
 @login_required(login_url='login')
